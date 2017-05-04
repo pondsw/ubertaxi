fc4b6d4c598e2592576ba671529cbbe01cd7fb04
Unknown <nathaporn.w@ku.th>
2017-05-04 19:31:34 +0700

    Add promotion controller and api

---
+++ app/Http/Controllers/Api/PromotionsController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PromotionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $promotions = \App\Promotion::all();
+        return [
+            'success' => true,
+            'data' => $promotions
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $promotion = new \App\Promotion;
+        $promotion->name = trim($request->name);
+        $promotion->detail = trim($request->detail);
+        $promotion->start_date = $request->start_date;
+        $promotion->exp_date = $request->exp_date;
+
+        if (!empty($promotion->name) && !empty($promotion->detail) && $promotion->save()){
+            return [
+                'success' => true,
+                'data' => "Promotion '{$promotion->name}' was saved with id: {$promotion->id}",
+                'id' => $promotion->id
+            ];
+        } else {
+            return [
+                'success' => false,
+                'data' => "Some error occurred"
+            ];
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $promotion = \App\Promotion::find($id);
+        if (!is_null($promotion))
+            return [
+                'success' => true,
+                'data' => $promotion
+            ];
+        return [
+            'success' => false,
+            'data' => 'Promotion not found'
+        ];
+    }
+
+    public function discount($id)
+    {
+        $promotion = \App\Promotion::find($id);
+        if (!is_null($promotion)) {
+            return [
+                'success' => true,
+                'data' => $promotion->discount()->get()
+            ];
+        } else {
+            return [
+                'success' => false,
+                'data' => 'Promotion not found'
+            ];
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
