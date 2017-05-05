0555ca927c2b0c751bb0df1094e449f61b80570d
DESKTOP-3DJLVHI\pondsw <sitthidech.p@gmail.com>
2017-05-05 23:06:12 +0700

    update voucher modal

--- resources/views/vouchers/index.blade.php
+++ resources/views/vouchers/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
+<div class="container" id='vue-app-vouchers'>
      @if (Auth::user())
         <hr>
         <div class="row">
@@ -25,18 +26,18 @@
         <!-- /.row -->
 
         <!-- Page Features -->
-        <div class="row text-center" id='vue-app-vouchers'>
+        <div class="row text-center" >
 
 
-            <div class="col-md-3 col-sm-6 hero-feature" v-for="d in data">
+            <div class="col-md-3 col-sm-6 hero-feature" v-for="d in dataVoucher">
             <!-- <div class="col-md-3 col-sm-6 hero-feature"> -->
                 <div class="thumbnail">
-                    <img :src="'http://ubertaxi.dev/storage/vouchers/' + d.id +'.jpg'" />
+                    <img :src="'http://ubertaxi.dev/storage/vouchers/' + d.id +'.jpg'"  height="200" >
                     <div class="caption">
                         <h3>Feature Label</h3>
                         <p>@{{ checkDetail(d.detail) }}</p>
                         <p>
-                            <a href="#" class="btn btn-primary">Get Now!</a> <a href="#" class="btn btn-default" data-toggle="modal" data-target="#myModal">More Info</a>
+                            <a href="#" class="btn btn-primary">Get Now!</a> <a href="#" class="btn btn-default" v-bind:id="d.id" v-on:click="greet" data-toggle="modal" data-target="#myModal">More Info</a>
                         </p>
                     </div>
                 </div>
@@ -44,6 +45,7 @@
 
         </div>
         <!-- /.row -->
+
 
         <!-- Modal -->
         <div class="modal fade" id="myModal" role="dialog">
@@ -56,7 +58,17 @@
                 <h4 class="modal-title">Modal Header</h4>
               </div>
               <div class="modal-body">
-                <p>Some text in the modal.</p>
+                <!-- <p>@{{ detail }}</p>
+                <p>@{{ exp_date }}</p> -->
+                <img :src="'http://ubertaxi.dev/storage/vouchers/' + id +'.jpg'"  height="200" >
+                <div class="row">
+                  <div class="col-sm-3">Detail</div>
+                  <div class="col-sm-9">@{{ detail }}</div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-3">Expiration Date </div>
+                  <div class="col-sm-9">@{{ exp_date }}</div>
+                </div>
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -65,6 +77,7 @@
 
           </div>
         </div>
+      </div>
 @endsection
 
 @section('script')
@@ -82,17 +95,38 @@
 
     var vm = new Vue({
         el: '#vue-app-vouchers',
-        data: data ,
+        data:{
+          dataVoucher : data.data,
+          showModal : false,
+          detail : "",
+          exp_date :"",
+          id : 1
+        },
         methods:{
-          checkDetail(contend) {
+          checkDetail: function(contend) {
             if(contend.length > 30){
               return contend.substring(0, 30) + "...";
             }else {
               return contend;
             }
-            // return '../assets/' + this.pics[index] + '.png';
+          },
+          greet: function (event) {
+            showModal = true;
+            // `this` inside methods points to the Vue instance
+            // alert('Hello ' + showModal + '!');
+            // `event` is the native DOM event
+            if (event) {
+              voucher = this.dataVoucher[event.target.id - 1];
+              // alert(event.target.id);
+              this.id = voucher.id;
+              this.detail = voucher.detail;
+              this.exp_date = voucher.exp_date;
+
+            }
           }
         }
     });
 </script>
+
+
 @endsection
