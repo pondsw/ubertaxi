8fa4377022dbee6d749e621710b0f4f0ec9853d6
Unknown <tanaboon.p@ku.th>
2017-05-08 22:53:48 +0700

    change id

--- resources/views/redeem.blade.php
+++ resources/views/redeem.blade.php
@@ -20,18 +20,18 @@
     <div class="row text-center" >
       <form class="form-horizontal">
         <div class="form-group">
-          <label for="inputEmail3" class="col-sm-2 control-label">Promotion Code</label>
+          <label for="Promotion Code" class="col-sm-2 control-label">Promotion Code</label>
           <div class="col-sm-8">
-            <input type="Code" class="form-control" id="inputEmail3" placeholder="Code">
+            <input type="Code" class="form-control" id="Promotion Code" placeholder="Code">
           </div>
           <div class="col-sm-2">
             <button type="submit" class="btn btn-default">Redeem</button>
           </div>
         </div>
         <div class="form-group">
-          <label for="inputPassword3" class="col-sm-2 control-label">Voucher Code</label>
+          <label for="Voucher Code" class="col-sm-2 control-label">Voucher Code</label>
           <div class="col-sm-8">
-            <input type="Code" class="form-control" id="inputPassword3" placeholder="Code">
+            <input type="Code" class="form-control" id="Voucher Code" placeholder="Code">
           </div>
           <div class="col-sm-2">
             <button type="submit" class="btn btn-default">Redeem</button>
