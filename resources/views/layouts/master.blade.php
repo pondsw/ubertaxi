11420cf8aa7eae6cee988deff45687f76d0f5f9b
Unknown <nutchapol_11@hotmail.com>
2017-05-09 03:01:35 +0700

    add css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,17 @@
         <meta charset="utf-8">
         <title>{{ $title or "REST API" }}</title>
         <link rel="stylesheet" href="/css/app.css">
+        <meta name = "format-detection" content = "telephone=no" />
+        <link rel="shortcut icon" href="images/favicon.ico" />
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery-migrate-1.2.1.js"></script>
+        <script src="js/script.js"></script>
+        <script src="js/superfish.js"></script>
+        <script src="js/jquery.ui.totop.js"></script>
+        <script src="js/jquery.equalheights.js"></script>
+        <script src="js/jquery.mobilemenu.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
     </head>
     <body>
         <div id ="app">
