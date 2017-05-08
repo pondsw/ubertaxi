9b07cf34f07cf20cc7852b12e9c4bbc25834cbe7
Unknown <tanaboon.p@ku.th>
2017-05-08 22:49:27 +0700

    view new redeem

--- resources/views/redeem.blade.php
+++ resources/views/redeem.blade.php
@@ -18,9 +18,25 @@
 
     <!-- Page Features -->
     <div class="row text-center" >
-      <form action="/action_page.php">
-        CODE : <input type="text" name="code" value="">
-        <input type="submit" value="Submit">
+      <form class="form-horizontal">
+        <div class="form-group">
+          <label for="inputEmail3" class="col-sm-2 control-label">Promotion Code</label>
+          <div class="col-sm-8">
+            <input type="Code" class="form-control" id="inputEmail3" placeholder="Code">
+          </div>
+          <div class="col-sm-2">
+            <button type="submit" class="btn btn-default">Redeem</button>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="inputPassword3" class="col-sm-2 control-label">Voucher Code</label>
+          <div class="col-sm-8">
+            <input type="Code" class="form-control" id="inputPassword3" placeholder="Code">
+          </div>
+          <div class="col-sm-2">
+            <button type="submit" class="btn btn-default">Redeem</button>
+          </div>
+        </div>
       </form>
     </div>
     <!-- /.row -->
